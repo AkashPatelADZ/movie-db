46878b172ca6bf2be9c5ab02d6dde27153df98b8
Akash Patel <akashpatel.profile@gmail.com>
2022-01-15 19:46:34 +0530

    Style 404

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./PageNotFound.scss";
 
 function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="page404">
+      <img src="./images/404_guava.png" alt="404" />
+      <a href="https://www.vecteezy.com/free-vector/404">
+        404 Vectors by Vecteezy
+      </a>
+    </div>
+  );
 }
 
 export default PageNotFound;
