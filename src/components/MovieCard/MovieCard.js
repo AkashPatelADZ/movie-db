aa2e27430d974e5ee6abb3d69da2aea05e1f12d6
Akash Patel <akashpatel.profile@gmail.com>
2022-01-26 12:38:57 +0530

    Fix MovieDetail

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,7 +5,7 @@ import "./MovieCard.scss";
 const MovieCard = ({ data }) => {
   return (
     <div className="card-item">
-      <Link to={`/movie/${data.imdbID}`}>
+      <Link to={`/movie-db/movie/${data.imdbID}`}>
         <div className="card-inner">
           <div className="card-top">
             <img src={data.Poster} alt={data.Title} />
